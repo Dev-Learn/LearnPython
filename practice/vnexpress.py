ca5876d5e97f4e608a57ebd8c64413c25de85967
Nam Tran <namtran09061992@gmail.com>
2018-09-27 00:49:59 +0700

    - Continue practice

--- practice/vnexpress.py
+++ practice/vnexpress.py
@@ -1,14 +1,45 @@
 """
     Chương trình tải các bài viết từ tuổi trẻ
 """
-from pip._vendor import requests
+import bs4
+import requests
 
 SOURCE_URL = 'https://vnexpress.net/tin-tuc/khoa-hoc'
+
+def get_content_detail(url):
+
+    """
+        Lấy content bài viết detail
+    """
+
+    print(url)
+
+    data = {}
+    r = requests.get(url)
+    if r.ok:
+        s = bs4.BeautifulSoup(r.content, 'lxml')
+        title = s.select_one('h1')
+        # print(title.text.strip() if title else '')
+        data['title'] = title
+        description = s.select_one('h2')
+        # print(description.text.strip() if description else '')
+        data['description'] = description
+        content = s.select_one('article')
+        # print(content.text.strip() if  content else '')
+        data['content'] = description
+
+    return data
 
 def main():
     r = requests.get(SOURCE_URL)
     if r.ok:
-        print(r.content)
+        s = bs4.BeautifulSoup(r.content,'lxml')
+        items = s.select('.list_news h3 > a')
+        for item in items:
+            if 'html#box_comment' in item.attrs['href']:
+               pass
+            else:
+                article = get_content_detail(item.attrs['href'])
     else:
         print('Không truy cập được !!!')
 
