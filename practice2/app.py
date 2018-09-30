862042179ed47304e9ec2be0ec0a103a07dd2f3a
Nam Tran <namtran09061992@gmail.com>
2018-09-30 20:07:03 +0700

    - Continue practice flask

--- practice2/app.py
+++ practice2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for,render_template
 
 # http://jinja.pocoo.org/docs/2.10/ ( template )
 
@@ -13,3 +13,15 @@ def test():
 @app.route('/')
 def index():
     return 'The static url is : %s' % (url_for('static', filename='js/main.js'))
+
+@app.route('/testtemplate')
+def testTemplate():
+    nav = [
+            # {"href" : url_for("testTemplate") ,"caption" : "Test Render Template Page"},
+            {"href" : url_for("test") ,"caption" : "Hello Page"}
+           ]
+    return render_template('test_template.html',val = 'Test',nav = nav)
+
+@app.route('/testExtendTemplate')
+def testExtendTemplate():
+    return render_template('test_extend_template.html')
