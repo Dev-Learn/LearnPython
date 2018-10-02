2a7bc0b3bfcbd60a83d9431b570cd483b0763502
Nam Tran <namtran09061992@gmail.com>
2018-10-02 18:52:30 +0700

    - Complete practice parse 600 word toiec

--- practice/vnexpress.py
+++ practice/vnexpress.py
@@ -16,7 +16,10 @@ def get_content_detail(url):
         Lấy content bài viết detail
     """
 
-    # print(url)
+    print(url)
+
+    if 'https://vnexpress.net/tin-tuc/khoa-hoc/trong-nuoc/khoa-hoc-viet-tach-chat-quy-tu-la-sen-dau-xanh-giup-giam-cholesterol-mau-3805348.html' == url:
+        a = 'b'
 
     data = {}
     r = requests.get(url)
@@ -36,22 +39,31 @@ def get_content_detail(url):
         data['content'] = content.prettify() if content else ''
 
         pub_date = s.select_one('span.time.left')
-        pub_date = pub_date.text.replace(' (GMT+7)','').split(',')[1] + pub_date.text.replace(' (GMT+7)','').split(',')[2] if  pub_date else ''
+        if not pub_date :
+            pub_date = s.select_one('div.time_post_art > span')
+            if not pub_date :
+                pub_date = s.select_one('header.clearfix > p')
+        pub_date = pub_date.text.replace(' (GMT+7)', '').split(',')[1] + \
+                   pub_date.text.replace(' (GMT+7)', '').split(',')[2] if pub_date else ''
         # print(pub_date.text.strip() if  pub_date else '')
         # print(pub_date.strip().split('/'))
-        if pub_date.strip().split('/')[1].strip().__len__() == 1 and pub_date.split('/')[0].strip().__len__() == 2:
-            pub_date = arrow.get(pub_date,'DD/M/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
-            print('DD/M/YYYY HH:mm')
-        elif pub_date.strip().split('/')[1].strip().__len__() == 2 and pub_date.split('/')[0].strip().__len__() == 1:
-            pub_date = arrow.get(pub_date, 'D/MM/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
-            print('D/MM/YYYY HH:mm')
-        elif pub_date.strip().split('/')[1] if pub_date.strip().split('/')[1].strip().__len__() == 1 else '' and pub_date.split('/')[0].strip().__len__() == 1:
-            pub_date = arrow.get(pub_date, 'D/M/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
-            print('D/M/YYYY HH:mm')
-        else:
-            pub_date = arrow.get(pub_date, 'DD/MM/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
-            print('DD/MM/YYYY HH:mm')
-        data['pub_date'] = pub_date.format(locale='vi')
+        print(pub_date)
+        if pub_date.split('/').__len__() >= 2:
+            day = pub_date.split('/')[0].strip()
+            month = pub_date.strip().split('/')[1].strip()
+            if day.__len__() == 2 and month.__len__() == 1:
+                pub_date = arrow.get(pub_date, 'DD/M/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
+                print('DD/M/YYYY HH:mm')
+            elif day.__len__() == 1 and month.__len__() == 2:
+                pub_date = arrow.get(pub_date, 'D/MM/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
+                print('D/MM/YYYY HH:mm')
+            elif day.__len__() == 1 and month.__len__() == 1:
+                pub_date = arrow.get(pub_date, 'D/M/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
+                print('D/M/YYYY HH:mm')
+            else:
+                pub_date = arrow.get(pub_date, 'DD/MM/YYYY HH:mm').replace(tzinfo='Asia/Ho_Chi_Minh')
+                print('DD/MM/YYYY HH:mm')
+            data['pub_date'] = pub_date.format(locale='vi')
 
     return data
 
@@ -65,19 +77,22 @@ def main():
             if 'html#box_comment' not in item.attrs['href']:
                 article = get_content_detail(item.attrs['href'])
                 # print(article)
-                dir_name = arrow.get(article['pub_date']).format('MM-YYYY')
+                if 'pub_date' in article.keys():
+                    dir_name = arrow.get(article['pub_date']).format('MM-YYYY')
 
-                # tạo thư mục
-                if not os.path.isdir(dir_name):
-                    os.mkdir(dir_name)
+                    # tạo thư mục
+                    if not os.path.isdir(dir_name):
+                        os.mkdir(dir_name)
 
-                # tạo tên tệp
-                file_name = arrow.get(article['pub_date']).format('DD-MM-YYYY-HH-mm')
-                article['id'] = file_name
-                file_name = os.path.join(dir_name,file_name)
-                f = codecs.open(file_name + '.json', encoding='utf-8', mode='w')
-                json.dump(article, f, ensure_ascii=False, indent=2)
+                    # tạo tên tệp
 
+                    file_name = arrow.get(article['pub_date']).format('DD-MM-YYYY-HH-mm')
+                    article['id'] = file_name
+                    file_name = os.path.join(dir_name, file_name)
+                    f = codecs.open(file_name + '.json', encoding='utf-8', mode='w')
+                    json.dump(article, f, ensure_ascii=False, indent=2)
+                else:
+                    print('"pub_date" not in article.keys()')
     else:
         print('Không truy cập được !!!')
 
--- practice4/main.py
+++ practice4/main.py
@@ -5,38 +5,195 @@
 import os
 import bs4
 import requests
+import shutil
 import json
 import codecs
-import arrow
 
 SOURCE_URL = 'https://600tuvungtoeic.com/'
 
+idWord = 1
+
 def main():
+
     r = requests.get(SOURCE_URL)
     if r.ok:
         s = bs4.BeautifulSoup(r.content, 'lxml')
-        print(s)
         items = s.select('.gallery-item')
-        data = {}
+        listTopic = []
+        listWord = []
+        listWordError = []
+        count = 1
         for item in items:
-            print('----------------------------------------------------------------------------------------------------------------------------------------')
+            count += 1
+            data = {}
+            print(
+                '----------------------------------------------------------------------------------------------------------------------------------------')
             # print(item)
             page = item.select_one('a')
             page = page.attrs['href'] if page else ''
 
-            image = item.select_one('img')
-            data['image'] = image.attrs['src'] if image else ''
+            topic_en = item.select_one('h3')
+            topic_en = topic_en.text.strip() if topic_en else ''
 
-            title = item.select_one('h3')
-            title = title.text.strip() if title else ''
+            data['topic_en'] = topic_en.split(' - ')[1] if topic_en else ''
+            data['id'] = int(topic_en.split(' - ')[0]) if topic_en else '0'
 
-            data['title'] = title.split(' - ')[1] if title else ''
-            data['id'] = title.split(' - ')[0] if title else ''
+            # print(page)
+            # print(str(data))
 
-            print(page)
-            print(str(data))
+            image_url = item.select_one('img')
+            image_url = image_url.attrs['src'] if image_url else ''
 
-            print('----------------------------------------------------------------------------------------------------------------------------------------')
+            data['image'] = downloadFile('topic_image', image_url)
+
+            topicDetail(page, data,listWord,listWordError)
+
+            listTopic.append(data)
+
+            print(
+                '---------------------------------------------------------------------------------------------------------------------------------------')
+
+            # if count == 5:
+            #     break
+        print(listTopic)
+        print(listWord)
+        dir = '600WordToiec'
+
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
+
+        topic = codecs.open(os.path.join(dir,'Topic.json'), encoding='utf-8', mode='w')
+        word = codecs.open(os.path.join(dir,'Word.json'), encoding='utf-8', mode='w')
+        wordError = codecs.open(os.path.join(dir,'WordError.json'), encoding='utf-8', mode='w')
+        json.dump(listTopic, topic, ensure_ascii=False, indent=2)
+        json.dump(listWord, word, ensure_ascii=False, indent=2)
+        json.dump(listWordError, wordError, ensure_ascii=False, indent=2)
+
+
+def topicDetail(url, topic,listWord,listError):
+    global idWord
+    isError = False
+    r = requests.get(os.path.join(SOURCE_URL,url))
+    print(r.url)
+    if r.ok:
+        s = bs4.BeautifulSoup(r.content, 'lxml')
+
+        topic_vi = s.select_one('h2.page-title')
+        topic_vi = topic_vi.text.strip() if topic_vi else ''
+        topic_vi = topic_vi.split(' - ')[-1]
+        # print(topic_vi)
+        topic['topic_vi'] = topic_vi
+
+        items = s.select('.tuvung')
+
+        for item in items:
+            word = {'id': idWord, 'id_topic': topic['id']}
+
+            contents = item.select('.noidung > span')
+            if contents.__len__() == 0:
+                continue
+            vocabulary = contents[0].text.strip()
+            vocabulary = vocabulary if vocabulary else ''
+            word['vocabulary'] = vocabulary
+
+            spelling = contents[1].text.strip()
+            spelling = spelling if spelling else ''
+            word['spelling'] = spelling
+
+            contain_explain_vi = contents[3].next_sibling.strip()
+            contain_explain_vi = contain_explain_vi if contain_explain_vi else ''
+
+            from_type = contain_explain_vi.split(')')[0]
+            from_type = from_type if from_type else ''
+            word['from_type'] = from_type.strip() + ')'
+
+            explain_vi = contain_explain_vi.split('):')[1]
+            explain_vi = explain_vi if explain_vi else ''
+            word['explain_vi'] = explain_vi.strip()
+
+            try:
+                explain_en = contents[2].next_sibling
+                explain_en = explain_en if explain_en else ''
+                if '<br/>' in str(explain_en):
+                    if 'catalog' in word['vocabulary']:
+                        explain_en = 'A complete list of items, typically one in alphabetical or other systematic order'
+                    elif 'open to' in word['vocabulary']:
+                        explain_en = 'To be receptive to or welcoming of something that comes from outside of oneself.'
+                    elif 'broaden' in word['vocabulary']:
+                        explain_en = 'Become larger in distance from side to side; widen.'
+                    elif 'be ready for' in word['vocabulary']:
+                        explain_en = 'feeling that you must have or must do something'
+                    else:
+                        example_en = ''
+                        isError = True
+                word['explain_en'] = explain_en
+            except TypeError:
+                continue
+
+            example_en = contents[4].next_sibling.strip()
+            example_en = example_en if example_en else ''
+            word['example_en'] = example_en
+
+            example_vi = item.select_one('.noidung > b')
+            example_vi = example_vi.text.strip() if example_vi else ''
+            word['example_vi'] = example_vi
+
+            image_word = item.select_one('.hinhanh > img')
+            image_word = image_word.attrs['src'] if image_word else ''
+            if image_word:
+                if ' ' in image_word:
+                    image_word = image_word.replace(' ', '%')
+                word['image'] = downloadFile('word_image', image_word)
+            else:
+                isError = True
+
+            audio = item.select_one('.noidung > audio > source')
+            audio = audio.attrs['src']  if audio else ''
+            if audio:
+                if ' ' in audio:
+                    audio = audio.replace(' ', '%')
+                word['audio'] = downloadFile('audio',os.path.join(SOURCE_URL,audio))
+            else:
+                isError = True
+                contentAudio = item.select_one('.noidung').contents[-1]
+                contentAudio = str(contentAudio) if contentAudio else ''
+                if contentAudio:
+                    contentAudio = contentAudio.split('<audio')[1]
+                    if contentAudio:
+                        contentAudio = contentAudio.split('<source src="')[1]
+                        if contentAudio:
+                            contentAudio = contentAudio.split('" type')[0]
+                            if ' ' in contentAudio:
+                                contentAudio = contentAudio.replace(' ', '%')
+                            word['audio'] = downloadFile('audio', os.path.join(SOURCE_URL, contentAudio))
+                            isError = False
+            idWord += 1
+
+            print(word)
+            if isError:
+                isError = False
+                listError.append(word)
+            else:
+                listWord.append(word)
+
+def downloadFile(dir, url):
+    # tạo thư mục
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+    file_name = url.split('/')[-1]
+    if '-' in file_name:
+        file_name = file_name.replace('-',"_")
+
+    if os.path.exists(file_name):
+        return file_name
+
+    response = requests.get(url, stream=True)
+    with open(dir + '/' + file_name, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+
+    return file_name
+
 
 if __name__ == '__main__':
     main()
