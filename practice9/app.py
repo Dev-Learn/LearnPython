d731f3ec12370a82ab61c5c178bd468d641892f3
trannam <namtran09061992@gmail.com>
2018-11-06 17:33:45 +0700

    - Fix some issue practice 9

--- practice9/app.py
+++ practice9/app.py
@@ -17,7 +17,7 @@ connection = pymysql.connect(host='localhost',
 
 
 def main():
-    driver = webdriver.Chrome('E:/chromedriver_win32/chromedriver.exe')
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
     driver.implicitly_wait(30)
     driver.get(SOURCE_URL)
     source = driver.page_source
@@ -28,7 +28,7 @@ def main():
 def clickNextPage(source, driver):
     page = getData(source)
     print(page)
-    if page == '11':
+    if page == '4':
         print('close')
         return
     driver.find_element_by_xpath("//a[contains(@href, 'LoadListMangaPage(%s)')]" % str(int(page) + 1)).click()
@@ -43,21 +43,21 @@ def getData(get):
     page = s.select_one('div.paging > span.current_page').text
     items = s.select('div.list > p > span.tiptip.fs-12.ellipsis')
     for item in items:
+        title = item.select_one('a').text
         link = item.select_one('a')
         link = link.attrs['href'] if link else ''
-        data.append(link)
+        data.append({'title' : title,'link': link})
 
     return page
 
 
-def getComic(link):
-    r = requests.get(link)
+def getComic(data):
+    r = requests.get("https://blogtruyen.com/%s" % data['link'])
     # print(r.url)
     comic = {}
+    comic['title'] = data['title']
     if r.ok:
         s = bs4.BeautifulSoup(r.content, 'lxml')
-        title = s.select_one('h1.entry-title').text
-        comic['title'] = title
         description = s.select_one('div.detail > div.content').text
         comic['description'] = description
         genre = []
@@ -68,7 +68,7 @@ def getComic(link):
         listImages = []
         for i in range(images.__len__() - 1,-1,-1):
             linkImage = images[i].select_one('a')
-            linkImage = linkImage.attrs['href'] if link else ''
+            linkImage = linkImage.attrs['href'] if linkImage else ''
             listImages.append(getImagesComic("https://blogtruyen.com/%s" % linkImage))
         comic['linkImages'] = listImages
     return comic
@@ -99,27 +99,30 @@ if __name__ == '__main__':
             mycursor.execute(
                 'CREATE TABLE IF NOT EXISTS genre (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, idcomic INT ,genre VARCHAR(255))')
             mycursor.execute(
-                'CREATE TABLE IF NOT EXISTS comic (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), description VARCHAR(255))')
-            for link in data:
+                'CREATE TABLE IF NOT EXISTS comic (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), description VARCHAR(255)) DEFAULT CHARSET=utf8')
+            for item in data:
                 # print('Link : ' + link)
-                comic = getComic("https://blogtruyen.com/%s" % link)
-                sql = 'INSERT INTO comic (title,description) VALUES (%s, %s)'
-                val = (comic['title'], comic['description'])
-                mycursor.execute(sql, val)
-                connection.commit()
-                id = mycursor.lastrowid
-                if id:
-                    for item in comic['genre']:
-                        sql = 'INSERT INTO genre (idcomic,genre) VALUES (%s, %s)'
-                        val = (id, item)
-                        mycursor.execute(sql, val)
-                        connection.commit()
+                try:
+                    comic = getComic(item)
+                    sql = 'INSERT INTO comic (title,description) VALUES (%s, %s)'
+                    val = (comic['title'], comic['description'])
+                    mycursor.execute(sql, val)
+                    connection.commit()
+                    id = mycursor.lastrowid
+                    if id:
+                        for item in comic['genre']:
+                            sql = 'INSERT INTO genre (idcomic,genre) VALUES (%s, %s)'
+                            val = (id, item)
+                            mycursor.execute(sql, val)
+                            connection.commit()
 
-                    for item in comic['linkImages']:
-                        sql = 'INSERT INTO linkimage (idcomic,image) VALUES (%s, %s)'
-                        val = (id, item)
-                        mycursor.execute(sql, val)
-                        connection.commit()
+                        for item in comic['linkImages']:
+                            sql = 'INSERT INTO linkimage (idcomic,image) VALUES (%s, %s)'
+                            val = (id, item)
+                            mycursor.execute(sql, val)
+                            connection.commit()
+                except Exception as e:
+                    print('Error : %s' % str(e))
 
         finally:
             connection.close()
