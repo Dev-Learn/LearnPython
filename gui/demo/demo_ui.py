6b402bac6bdedbff1a17c4ba49331730f5df1df8
trannam <namtran09061992@gmail.com>
2019-06-06 14:23:36 +0700

    - Complete demo sample pyqt file browser

---
+++ gui/demo/demo_ui.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'demo.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.fileBrower = QtWidgets.QTreeView(self.frame)
+        self.fileBrower.setObjectName("fileBrower")
+        self.gridLayout_2.addWidget(self.fileBrower, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
---
+++ gui/demo/fileBrowser.py
@@ -0,0 +1,50 @@
+import sys
+
+import subprocess, os, platform
+
+import os
+from PyQt5 import QtWidgets,QtCore,QtGui
+from gui.demo import demo_ui
+
+class FileBrowser(demo_ui.Ui_MainWindow,QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(FileBrowser,self).__init__()
+        self.setupUi(self)
+        self.fileBrower.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.fileBrower.customContextMenuRequested.connect(self.context_menu)
+        self.populate()
+
+    def populate(self):
+        path = "/Users/trannam"
+        self.model = QtWidgets.QFileSystemModel()
+        self.model.setRootPath((QtCore.QDir.rootPath()))
+        self.fileBrower.setModel(self.model)
+        self.fileBrower.setRootIndex(self.model.index(path))
+        self.fileBrower.setSortingEnabled(True)
+
+    def context_menu(self):
+        menu = QtWidgets.QMenu()
+        open = menu.addAction("Open")
+        open.triggered.connect(self.openFile)
+        cursor = QtGui.QCursor()
+        menu.exec_(cursor.pos())
+
+    def openFile(self):
+        index = self.fileBrower.currentIndex()
+        path = self.model.filePath(index)
+        name = self.model.fileName(index)
+        print(path)
+        print(name)
+        if platform.system() == 'Darwin':  # macOS
+            subprocess.call(('open', path))
+        elif platform.system() == 'Windows':  # Windows
+            os.startfile(path)
+        else:  # linux variants
+            subprocess.call(('xdg-open', path))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    fileBrowser = FileBrowser()
+    fileBrowser.show()
+    sys.exit(app.exec_())
